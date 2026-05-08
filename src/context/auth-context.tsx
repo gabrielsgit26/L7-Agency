13086556d590b4e2149266c36c99ac33212842f1
gabrielsgit26 <testjio@mail.com>
2026-05-08 04:30:18 -0700

    feat: update

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -78,6 +78,7 @@ export function AuthProvider({
           try {
             setLoading(true)
             // User logged in
+            console.log('Auth State Changed:', firebaseUser)
             if (firebaseUser) {
               // TEMP ROLE
               getUserProfile(firebaseUser.uid).then((result) => {
